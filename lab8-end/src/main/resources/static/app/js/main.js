2c588b05666c22875dfb133204a0a2f4a3842d3c
nenadtodEdu <nenadtod@uns.ac.rs>
2019-07-02 21:04:22 +0200

    lab8-end

---
+++ lab8-end/src/main/resources/static/app/js/main.js
@@ -0,0 +1,302 @@
+var wafepaApp = angular.module("wafepa", ["ngRoute"]);
+
+wafepaApp.controller("HomeCtrl", function($scope){
+	$scope.message = "Hello JWD!";
+});
+
+
+wafepaApp.controller("ActivitiesCtrl", function($scope, $http, $location){
+	
+	var baseUrl = "/api/activities";
+	
+	$scope.activities = [];
+	
+	var getActivities = function(){
+		var promise = $http.get(baseUrl);
+		promise.then(
+			function success(res){
+				//console.log(res);
+				$scope.activities = res.data;
+			},
+			function error(res){
+				
+			}
+		);
+		
+		//console.log("test");
+	}
+	
+	getActivities();
+	
+	
+	$scope.goToEdit = function(id){
+		$location.path("/activities/edit/" + id);
+	}
+	
+	$scope.goToAdd = function(){
+		$location.path("/activities/add");
+	}
+});
+
+
+wafepaApp.controller("EditActivityCtrl", function($scope, $http, $routeParams, $location){
+	//console.log($routeParams);
+	
+	var url = "/api/activities/" + $routeParams.id;
+	
+	$scope.activity = {};
+	$scope.activity.name = "";
+	
+	var getActivity = function(){
+		var promise = $http.get(url);
+		promise.then(
+			function success(res){
+				$scope.activity = res.data;
+			},
+			function error(){
+				alert("Couldn't fetch the activity");
+			}
+		);
+	}
+	
+	getActivity();
+	
+	$scope.doEdit = function(){
+		$http.put(url, $scope.activity).then(
+			function uspeh(){
+				$location.path("/activities");
+			},
+			function neuspeh(){
+				alert("Couldn't edit the activity.")
+			}
+		);
+	}
+
+});
+
+
+wafepaApp.controller("AddActivityCtrl", function($scope,$location,$http){
+	
+	var url = "/api/activities";
+	
+	$scope.activity = {};
+	$scope.activity.name = "";
+	
+	$scope.doAdd = function(){
+		$http.post(url, $scope.activity).then(
+			function success(){
+				$location.path("/activities");
+			},
+			function error(){
+				alert("Couldn't post the activity");
+			}
+		);
+	}
+	
+});
+
+
+wafepaApp.controller("RecordsCtrl", function($scope, $http, $location){
+	
+	var url = "/api/records";
+	var urlActivites = "/api/activities";
+	var urlUsers = "/api/users";
+	
+	
+	$scope.records = [];
+	$scope.activities = [];
+	$scope.users = [];
+	
+	
+	$scope.newRecord = {};
+	$scope.newRecord.time = "";
+	$scope.newRecord.duration = "";
+	$scope.newRecord.intensity = "";
+	$scope.newRecord.activityId = "";
+	$scope.newRecord.userId = "";
+	//TODO: dodati obeležja kojim se povezuje sa korisnikom i aktivnošću
+	
+	
+	var getRecords = function(){
+		var promise = $http.get(url);
+		promise.then(
+			function success(res){
+				$scope.records = res.data;
+			},
+			function error(){
+				alert("Couldn't fetch records");
+			}
+		);
+	}
+	
+	getRecords();
+	
+	//TODO: Obezbediti prihvat korisnika i aktivnosti
+	var getUsers = function(){
+		
+		var promise = $http.get(urlUsers);
+		promise.then(
+			function success(res){
+				$scope.users = res.data;
+			},
+			function error(res){
+				alert("Couldn't fetch users.")
+			}
+		);	
+		
+	}
+	
+	getUsers();
+	
+	var getActivities = function(){
+		
+		$http.get(urlActivites).then(
+			function uspeh(odg){
+				$scope.activities = odg.data;
+			},
+			function neuspeh(){
+				alert("Couln't fetch activites.");
+			}
+		);
+		
+	}
+	
+	getActivities();
+	
+	
+	
+	$scope.doAdd = function(){
+		
+		$http.post(url, $scope.newRecord).then(
+			function success(res){
+				getRecords();
+				
+				$scope.newRecord.time = "";
+				$scope.newRecord.duration = "";
+				$scope.newRecord.intensity = "";
+				$scope.newRecord.activityId = "";
+				$scope.newRecord.userId = "";
+			},
+			function error(){
+				alert("Couldn't save the record");
+			}
+		);
+	}
+	
+	$scope.goToEdit = function(id){
+		$location.path("/records/edit/" + id);
+	}
+	
+	$scope.doDelete = function(id){
+		$http.delete(url + "/" + id).then(
+			function success(res){
+				getRecords();
+			},
+			function error(res){
+				alert("Couln't delete the record.")
+			}
+		);
+	}
+	
+});
+
+
+wafepaApp.controller("EditRecordCtrl", function($scope, $http, $routeParams, $location){
+	
+	var recordUrl = "/api/records/" + $routeParams.id;
+	var activitiesUrl = "/api/activities";
+	var usersUrl = "/api/users";
+	
+	$scope.record = {};
+	$scope.record.time = "";
+	$scope.record.duration = "";
+	$scope.record.intensity = "";
+	$scope.record.userId = "";
+	$scope.record.activityId = "";	
+	
+	
+	$scope.activities = [];
+	$scope.users = [];
+	
+	
+	
+	var getActivities = function(){
+		$http.get(activitiesUrl).then(
+			function success(res){
+				$scope.activities = res.data;
+				
+				getUsers();
+			},
+			function error(){
+				alert("Couldn't fetch activities");
+			}
+		);
+	}
+	
+	var getUsers = function(){
+		return $http.get(usersUrl).then(
+			function success(res){
+				$scope.users = res.data;
+				
+				getRecord();
+			},
+			function error(){
+				alert("Couldn't fetch users.");
+			}
+		);
+	}
+	
+	var getRecord = function(){
+		$http.get(recordUrl).then(
+			function success(res){
+				$scope.record = res.data;
+			},
+			function error(){
+				alert("Couldn't fetch record.");
+			}
+		);
+	}
+	
+	//TODO: Obezbediti redosled izvrsavanja!
+	getActivities();
+	
+	
+	$scope.doEdit = function(){
+		$http.put(recordUrl, $scope.record).then(
+			function success(){
+				$location.path("/records");
+			},
+			function error(){
+				alert("Something went wrong.");
+			}
+		);
+	}
+});
+
+
+
+wafepaApp.config(['$routeProvider', function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl : '/app/html/home.html',
+			controller : 'HomeCtrl'
+		})
+		.when('/activities', {
+			templateUrl : '/app/html/activities.html'
+		})
+		.when('/activities/add', {
+			templateUrl : '/app/html/add-activity.html'
+		})
+		.when('/activities/edit/:id', {
+			templateUrl : '/app/html/edit-activity.html'
+		})
+		.when('/records', {
+			templateUrl : '/app/html/records.html'
+		})
+		.when('/records/edit/:id', {
+			templateUrl : '/app/html/edit-record.html'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+}]);
